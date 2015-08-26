1b63da41f2c97d2633c85303bb999db3dda26301
indefini <chris@indefini.org>
2015-08-26 09:20:41 +0900

    panel wip

---
+++ src/panel.h
@@ -0,0 +1,16 @@
+#ifndef __panel__
+#define __panel__
+#include <Elementary.h>
+
+typedef struct _Panel Panel;
+
+struct _Panel
+{
+  Evas_Object* close;
+  Evas_Object* reduce;;
+};
+
+//typedef void (rust_cb)(void* data);
+
+
+#endif
